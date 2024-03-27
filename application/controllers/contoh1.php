8f1ff71093b155c6ee647c52fdc94ea9b01ca7cb
renaldy05 <renaldyreshaa@gmail.com>
2024-03-27 14:35:39 +0700

    Tugas Pertemuan2

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -4,7 +4,7 @@ class Contoh1 extends CI_Controller
  public function index()
  {
     echo "<h1>Perkenalkan</h1>";
-    echo"Nama saya Imam Nawawi
+    echo"Nama saya Renaldy Resha Tambunan
     Saya tinggal di Narogong";
  }
 }
