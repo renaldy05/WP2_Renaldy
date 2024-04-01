d17cd73b150d73412c352ba8e82dd0e728ace55e
renaldy05 <renaldyreshaa@gmail.com>
2024-04-01 21:33:16 +0700

    Tugas Pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Renaldy Resha</li>
+ <li>Nama Belakang : Tambunan</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Taman Narogong Indah</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Bekasi</li>
+ </ul>
+ <h4>OlahRaga Favorit</h4>
+ <ul type="square">
+ <li>Bulutangkis</li>
+ </ul>
+</section>
